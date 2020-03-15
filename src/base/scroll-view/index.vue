09a76e0976068ae5a13fbe390b196dab6891d90b
e <1244653559@qq.com>
2020-03-15 15:09:10 +0800

    【优化】优化推荐页面样式以及功能

--- src/base/scroll-view/index.vue
+++ src/base/scroll-view/index.vue
@@ -12,6 +12,7 @@ const DIRECTION_V = 'vertical'
 
 export default {
   props: {
+    //
     probeType: {
       type: Number,
       default: 1
@@ -20,7 +21,18 @@ export default {
       type: Boolean,
       default: false
     },
+    // 是否监听滚动
     listenScroll: {
+      type: Boolean,
+      default: false
+    },
+    // 是否监听上拉加载
+    listenPullingUp: {
+      type: Boolean,
+      default: false
+    },
+    // 是否监听上拉加载
+    listenPullingDown: {
       type: Boolean,
       default: false
     },
@@ -40,9 +52,24 @@ export default {
       type: Number,
       default: 20
     },
+    // 滚动方向
     direction: {
       type: String,
       default: DIRECTION_V
+    },
+    // 上拉加载配置项
+    pullUpLoad: {
+      type: Object,
+      default: null
+    },
+    // 下拉刷新配置项
+    pullDownRefresh: {
+      type: [Boolean, Object],
+      default: false
+    },
+    scrollbar: {
+      type: [Boolean, Object],
+      default: false
     }
   },
   mounted() {
@@ -58,7 +85,10 @@ export default {
       this.scroll = new BScroll(this.$refs.wrapper, {
         probeType: this.probeType,
         click: this.click,
-        eventPassthrough: this.direction === DIRECTION_V ? DIRECTION_H : DIRECTION_V
+        eventPassthrough: this.direction === DIRECTION_V ? DIRECTION_H : DIRECTION_V,
+        scrollbar: this.scrollbar,
+        pullUpLoad: this.pullUpLoad,
+        pullDownRefresh: this.pullDownRefresh
       })
       // 滚动过程中，具体时机取决于选项中的 probeType。
       if (this.listenScroll) {
@@ -78,6 +108,21 @@ export default {
       if (this.beforeScroll) {
         this.scroll.on('beforeScrollStart', () => {
           this.$emit('beforeScroll')
+        })
+      }
+      // 上拉加载
+      if (this.listenPullingUp) {
+        this.scroll.on('pullingUp', () => {
+          this.$emit('pullingUp')
+        })
+      }
+      if (this.listenPullingDown) {
+        this.scroll.on('pullingDown', () => {
+          this.$emit('pullingDown')
+          // setTimeout(() => {
+          // // 事情做完，需要调用此方法告诉 better-scroll 数据已加载，否则下拉事件只会执行一次
+          //   this.scroll.finishPullDown()
+          // }, 2000)
         })
       }
     },
@@ -100,11 +145,22 @@ export default {
     // 滚动到指定的目标元素
     scrollToElement() {
       this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments)
+    },
+    // 完成下拉刷新
+    finishPullDown() {
+      this.scroll && this.scroll.finishPullDown()
+    },
+    // 完成上拉加载
+    finishPullUp() {
+      this.scroll && this.scroll.finishPullUp()
     }
   },
   watch: {
     data() {
       setTimeout(() => {
+        if (this.listenPullingUp) {
+          this.finishPullUp()
+        }
         this.refresh()
       }, this.refreshDelay)
     }
--- src/views/recommend/index.vue
+++ src/views/recommend/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="recommend">
-    <ScrollView class="recommend-content" ref="scroll" :data="discList" :listenPullingUp="listenPullingUp" :pullUpLoad="pullUpLoad" @pullingUp="pullingUp">
+    <ScrollView class="recommend-content" ref="scroll" :data="discList" :listenPullingUp="listenPullingUp" :scrollbar="scrollbar" :pullUpLoad="pullUpLoad" :listenPullingDown="listenPullingDown" :pullDownRefresh="pullDownRefresh" @pullingUp="pullingUp" @pullingDown="pullingDown">
       <div>
         <div v-if="sliderList.length" class="slider-wrapper">
           <div class="slider-content">
@@ -13,7 +13,7 @@
             </slider>
           </div>
         </div>
-        <div class="recommend-list">
+        <div class="recommend-list" v-if="discList.length>0">
           <h1 class="recommend-list-title">热门歌单推荐</h1>
           <ul>
             <li @click="selectItem(item)" v-for="(item,index) in discList" class="recommend-list-item" :key="index">
@@ -28,30 +28,44 @@
           </ul>
         </div>
       </div>
+      <div class="loading-container" v-show="!discList.length">
+        <loading />
+      </div>
     </ScrollView>
-
   </div>
 </template>
 
 <script>
 import { getSliderList, getDiscList } from 'api/recommend'
 import Slider from 'base/slider/'
-import ScrollView from '@/base/scroll-view/'
-
+import ScrollView from 'base/scroll-view/'
+import Loading from 'base/loading/'
 import { ERR_OK } from 'api/config'
 
 export default {
   name: 'recommend',
   components: {
-    Slider, ScrollView
+    Slider, ScrollView, Loading
   },
   data() {
     return {
       sliderList: [],
       discList: [],
+      // 上拉加载配置项
       pullUpLoad: {
         threshold: 20
-      }
+      },
+      // 滚动条配置项
+      scrollbar: {
+        fade: true
+      },
+      // 下拉刷新配置项
+      pullDownRefresh: {
+        threshold: 50,
+        stop: 20
+      },
+      listenPullingUp: true, // 是否监听上拉加载
+      listenPullingDown: true // 是否监听下拉刷新
     }
   },
   async created() {
@@ -82,11 +96,18 @@ export default {
         }, 20)
       }
     },
-    listenPullingUp() {
-
+    // 下拉刷新
+    pullingDown() {
+      console.log('dfasf')
+      this.$refs.scroll.finishPullDown()
     },
+    // 上拉加载
     pullingUp() {
       console.log('lai')
+    },
+    // 打开歌单
+    selectItem(item) {
+      console.log(item)
     }
   }
 }
